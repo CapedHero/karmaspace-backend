9eaf886a08250e5c6c6de66c530f63ac465b781d
Maciej Wrześniewski <mc.wrzesniewski@gmail.com>
2021-08-17 12:22:52 +0200

    Fix deployment

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -x  # Print each command
 set -e  # Exit immediately if a command returns a non-zero status
 
 curl -sL https://git.io/tusk | bash -s -- -b .venv/bin latest
+python -m pip install --upgrade pip
 python -m pip install -r requirements/locked/prod.txt
