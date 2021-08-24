bfa4ca9c79993a973c53d395abb98a34bd8c53c4
Maciej Wrześniewski <mc.wrzesniewski@gmail.com>
2021-08-24 21:59:29 +0200

    Add IsOwner permission

--- src/core/permissions.py
+++ src/core/permissions.py
@@ -1,6 +1,14 @@
+from typing import Protocol
+
 from django.views import View
 from rest_framework import permissions
 from rest_framework.request import Request
+
+from src.app_auth.models import User
+
+
+class HasOwner(Protocol):
+    owner: User
 
 
 class IsAccessingOwnResourceOrReadOnly(permissions.BasePermission):
@@ -9,3 +17,8 @@ class IsAccessingOwnResourceOrReadOnly(permissions.BasePermission):
             return True
         else:
             return request.user.username == view.kwargs.get("username")
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request: Request, view: View, obj: HasOwner) -> bool:
+        return obj.owner == request.user
