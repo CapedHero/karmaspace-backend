daaa1684538d4867d48466a570d00d1d5cf077a3
Maciej Wrześniewski <mc.wrzesniewski@gmail.com>
2021-09-07 17:45:44 +0200

    Remove KarmaBoard.slug field and constraint

--- src/karmaspace/models/karmaboard.py
+++ src/karmaspace/models/karmaboard.py
@@ -2,7 +2,6 @@ from typing import Any
 
 from django.contrib.postgres.fields import CICharField
 from django.db import models
-from django.utils.text import slugify
 
 from src.app_auth.models import User
 from src.core.models import BaseModel
@@ -20,7 +19,6 @@ class ValueStep(models.TextChoices):
 class KarmaBoard(BaseModel):
     owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="karmaboards")
     name = CICharField(max_length=30)
-    slug = models.SlugField(max_length=50)
     value_step = models.CharField(choices=ValueStep.choices, default=ValueStep.BY_1, max_length=20)
     sort_index = models.FloatField(blank=True)
 
@@ -33,20 +31,15 @@ class KarmaBoard(BaseModel):
                 fields=["owner", "name"],
                 name="unique_karmaboard_name_per_owner",
             ),
-            models.UniqueConstraint(
-                fields=["owner", "slug"],
-                name="unique_karmaboard_slug_per_owner",
-            ),
         ]
 
     def __repr__(self) -> str:
-        return get_object_str(self, attrs_to_show=["owner.username", "slug", "id"])
+        return get_object_str(self, attrs_to_show=["owner.username", "id"])
 
     def __str__(self) -> str:
-        return f"KarmaBoard {self.owner.username}/{self.slug} | #ID={self.id}"
+        return f"KarmaBoard {self.name} | #ID={self.id}"
 
     def save(self, *args: Any, **kwargs: Any) -> None:
-        self.slug = slugify(self.name)
         if not self.sort_index:
             self.sort_index = get_sort_index_for_last_position(
                 queryset=self.__class__.objects.filter(owner=self.owner)
