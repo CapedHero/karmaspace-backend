e99461196806a7ff13f17fa832f1dca26f7df648
Maciej Wrześniewski <mc.wrzesniewski@gmail.com>
2021-10-07 18:04:02 +0200

    Revamp fixtures

--- fixtures/models/karmaboards.py
+++ fixtures/models/karmaboards.py
@@ -1,32 +1,64 @@
-from typing import Tuple
+from dataclasses import dataclass
 
+from fixtures.models.unsplash_photos import Photos
 from src.app_auth.models import User
 from src.karmaspace.models import KarmaBoard
-from src.karmaspace.models.unsplash_photo import UnsplashPhoto
 
 
-def create_karmaboards(
-    user: User,
-    unsplash_photo_1: UnsplashPhoto,
-    unsplash_photo_2: UnsplashPhoto,
-    unsplash_photo_3: UnsplashPhoto,
-) -> Tuple[KarmaBoard, KarmaBoard, KarmaBoard]:
-    karmaboard_1 = KarmaBoard.objects.create(
-        owner=user,
-        name="Obowiązki domowe",
-        unsplash_photo=unsplash_photo_1,
+@dataclass
+class KarmaBoards:
+    duties_and_leasure: KarmaBoard
+    fitness_and_health: KarmaBoard
+    house_chores: KarmaBoard
+    learning_english: KarmaBoard
+    learning_guitar: KarmaBoard
+    plants: KarmaBoard
+    reading_books: KarmaBoard
+
+
+def create_karmaboards(user: User, photos: Photos) -> KarmaBoards:
+    karmaboards = KarmaBoards(
+        duties_and_leasure=KarmaBoard.objects.create(
+            owner=user,
+            name="Obowiązki i rozrywki Hani",
+            unsplash_photo=photos.sunflowers,
+            sort_index=0.1,
+        ),
+        fitness_and_health=KarmaBoard.objects.create(
+            owner=user,
+            name="Sport i zdrowie",
+            unsplash_photo=photos.bowl_food,
+            sort_index=0.3,
+        ),
+        house_chores=KarmaBoard.objects.create(
+            owner=user,
+            name="Dom i porządki",
+            unsplash_photo=photos.wooden_house,
+            sort_index=0.2,
+        ),
+        learning_english=KarmaBoard.objects.create(
+            owner=user,
+            name="Nauka angielskiego",
+            unsplash_photo=photos.english_flag,
+            sort_index=0.5,
+        ),
+        learning_guitar=KarmaBoard.objects.create(
+            owner=user,
+            name="Gitara",
+            unsplash_photo=photos.guitar,
+            sort_index=0.6,
+        ),
+        plants=KarmaBoard.objects.create(
+            owner=user,
+            name="Rośliny",
+            unsplash_photo=photos.succulents,
+            sort_index=0.4,
+        ),
+        reading_books=KarmaBoard.objects.create(
+            owner=user,
+            name="Czytanie książek",
+            unsplash_photo=photos.library,
+            sort_index=0.7,
+        ),
     )
-
-    karmaboard_2 = KarmaBoard.objects.create(
-        owner=user,
-        name="Rośliny i ogród",
-        unsplash_photo=unsplash_photo_2,
-    )
-
-    karmaboard_3 = KarmaBoard.objects.create(
-        owner=user,
-        name="KarmaSpace startup",
-        unsplash_photo=unsplash_photo_3,
-    )
-
-    return karmaboard_1, karmaboard_2, karmaboard_3
+    return karmaboards
--- fixtures/models/unsplash_photos.py
+++ fixtures/models/unsplash_photos.py
@@ -1,31 +1,79 @@
-from typing import Tuple
+from dataclasses import dataclass
 
 from src.karmaspace.models.unsplash_photo import UnsplashPhoto
 
 
-def create_unsplash_photos() -> Tuple[UnsplashPhoto, UnsplashPhoto, UnsplashPhoto]:
-    unsplash_photo_1 = UnsplashPhoto.objects.create(
-        id="HzpdExNKf8",
-        regular_url="https://images.unsplash.com/photo-1631119460950-583cd4801910?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MTV8WF92blp0MmZQend8fHx8fDJ8fDE2MzI3NzgwMzE&ixlib=rb-1.2.1&q=80&w=1080",
-        small_url="https://images.unsplash.com/photo-1631119460950-583cd4801910?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MTV8WF92blp0MmZQend8fHx8fDJ8fDE2MzI3NzgwMzE&ixlib=rb-1.2.1&q=80&w=400",
-        author_name="Colin Cassidy",
-        author_url="https://unsplash.com/@colincassidy",
+@dataclass
+class Photos:
+    bowl_food: UnsplashPhoto
+    english_flag: UnsplashPhoto
+    guitar: UnsplashPhoto
+    library: UnsplashPhoto
+    succulents: UnsplashPhoto
+    sunflowers: UnsplashPhoto
+    wooden_house: UnsplashPhoto
+
+
+def create_unsplash_photos() -> Photos:
+    bowl_food, _ = UnsplashPhoto.objects.get_or_create(
+        id="kcA-c3f_3FE",
+        regular_url="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1NDQ5Nw&ixlib=rb-1.2.1&q=80&w=1080",
+        small_url="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1NDQ5Nw&ixlib=rb-1.2.1&q=80&w=400",
+        author_name="Anh Nguyen",
+        author_url="https://unsplash.com/@pwign",
     )
 
-    unsplash_photo_2 = UnsplashPhoto.objects.create(
-        id="3i5PHVp1Fkw",
-        regular_url="https://images.unsplash.com/photo-1570641963303-92ce4845ed4c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MTh8WF92blp0MmZQend8fHx8fDJ8fDE2MzI3NzgwMzE&ixlib=rb-1.2.1&q=80&w=1080",
-        small_url="https://images.unsplash.com/photo-1570641963303-92ce4845ed4c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MTh8WF92blp0MmZQend8fHx8fDJ8fDE2MzI3NzgwMzE&ixlib=rb-1.2.1&q=80&w=400",
-        author_name="Massimiliano Morosinotto",
-        author_url="https://unsplash.com/@therawhunter",
+    english_flag, _ = UnsplashPhoto.objects.get_or_create(
+        id="C713D7KSkaY",
+        regular_url="https://images.unsplash.com/photo-1603989872628-7880d83bb581?crop=entropy&cs=tinysrgb"
+        "&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1MDUwMg&ixlib=rb-1.2.1&q=80&w=1080",
+        small_url="https://images.unsplash.com/photo-1603989872628-7880d83bb581?crop=entropy&cs=tinysrgb&fit=max&fm"
+        "=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1MDUwMg&ixlib=rb-1.2.1"
+        "&q=80&w=400",
+        author_name="Sigmund",
+        author_url="https://unsplash.com/@sigmund",
     )
 
-    unsplash_photo_3 = UnsplashPhoto.objects.create(
-        id="bXRxaGX9MsY",
-        regular_url="https://images.unsplash.com/photo-1626888637475-e42a70122b59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258NnxYX3ZuWnQyZlB6d3x8fHx8Mnx8MTYzMjc3ODAzMQ&ixlib=rb-1.2.1&q=80&w=1080",
-        small_url="https://images.unsplash.com/photo-1626888637475-e42a70122b59?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258NnxYX3ZuWnQyZlB6d3x8fHx8Mnx8MTYzMjc3ODAzMQ&ixlib=rb-1.2.1&q=80&w=400",
-        author_name="Daniel J. Schwarz",
-        author_url="https://unsplash.com/@danieljschwarz",
+    guitar, _ = UnsplashPhoto.objects.get_or_create(
+        id="bNKBFtg5vig",
+        regular_url="https://images.unsplash.com/photo-1559304754-a042e039e5dd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1NjcxNA&ixlib=rb-1.2.1&q=80&w=1080",
+        small_url="https://images.unsplash.com/photo-1559304754-a042e039e5dd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1NjcxNA&ixlib=rb-1.2.1&q=80&w=400",
+        author_name="Maxime Favier",
+        author_url="https://unsplash.com/@maximefavier",
     )
 
-    return unsplash_photo_1, unsplash_photo_2, unsplash_photo_3
+    library, _ = UnsplashPhoto.objects.get_or_create(
+        id="2JIvboGLeho",
+        regular_url="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258N3xmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1NTkyNQ&ixlib=rb-1.2.1&q=80&w=1080",
+        small_url="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258N3xmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1NTkyNQ&ixlib=rb-1.2.1&q=80&w=400",
+        author_name="Susan Q Yin",
+        author_url="https://unsplash.com/@syinq",
+    )
+
+    sunflowers, _ = UnsplashPhoto.objects.get_or_create(
+        id="79RuacUiXUI",
+        defaults={
+            "regular_url": "https://images.unsplash.com/photo-1552160793-cbaf3ebcba72?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU0MjI4MA&ixlib=rb-1.2.1&q=80&w=1080",
+            "small_url": "https://images.unsplash.com/photo-1552160793-cbaf3ebcba72?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU0MjI4MA&ixlib=rb-1.2.1&q=80&w=400",
+            "author_name": "Jordan Cormack",
+            "author_url": "https://unsplash.com/@jordancormack",
+        },
+    )
+
+    succulents, _ = UnsplashPhoto.objects.get_or_create(
+        id="8mqOw4DBBSg",
+        regular_url="https://images.unsplash.com/photo-1446071103084-c257b5f70672?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MnxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1MjgwMQ&ixlib=rb-1.2.1&q=80&w=1080",
+        small_url="https://images.unsplash.com/photo-1446071103084-c257b5f70672?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MnxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1MjgwMQ&ixlib=rb-1.2.1&q=80&w=400",
+        author_name="Annie Spratt",
+        author_url="https://unsplash.com/@anniespratt",
+    )
+
+    wooden_house, _ = UnsplashPhoto.objects.get_or_create(
+        id="tj2WkhP3D10",
+        regular_url="https://images.unsplash.com/photo-1625592315827-259d633f9f3a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1NTA4MA&ixlib=rb-1.2.1&q=80&w=1080",
+        small_url="https://images.unsplash.com/photo-1625592315827-259d633f9f3a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTkxMDh8MHwxfGNvbGxlY3Rpb258MXxmbHYzSk96TklDa3x8fHx8Mnx8MTYzMzU1NTA4MA&ixlib=rb-1.2.1&q=80&w=400",
+        author_name="Henri Lajarrige Lombard",
+        author_url="https://unsplash.com/@henri0019",
+    )
+
+    return Photos(bowl_food, english_flag, guitar, library, succulents, sunflowers, wooden_house)
