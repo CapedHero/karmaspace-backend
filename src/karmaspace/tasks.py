eec2dea400efbea2b4953be160675184f59c1116
Maciej Wrześniewski <mc.wrzesniewski@gmail.com>
2021-10-09 17:47:17 +0200

    Update sending user feedback logic

--- src/karmaspace/tasks.py
+++ src/karmaspace/tasks.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.template.loader import render_to_string
 
 import bleach
+from django.utils.html import strip_tags
 
 from src.core.dramatiq_actors import dramatiq_actor
 from src.core.email import send_email
@@ -30,15 +31,29 @@ def send_new_user_created_msg_to_karmaspace_team(user_username: str, user_email:
 
 @dramatiq_actor()
 def send_user_feedback(
-    username: str,
-    user_email: str,
     dangerous_user_msg: str,
+    user_pk: str = "",
 ) -> None:
+    is_empty_msg = len(strip_tags(dangerous_user_msg)) == 0
+    if is_empty_msg:
+        return
+
     safe_user_msg = bleach.clean(dangerous_user_msg, tags=settings.EMAIL_ALLOWED_HTML_TAGS)
     ready_to_send_user_msg = bleach.linkify(safe_user_msg)
 
+    if user_pk:
+        from src.app_auth.models import User  # Prevent circular imports
+
+        user = User.objects.get(pk=user_pk)
+        subject = f"Nowa wiadomość od użytkownika {user.username}"
+        ready_to_send_user_msg += (
+            f'<hr><div>Email użytkownika: <a href="mailto:{user.email}">{user.email}</a></div>'
+        )
+    else:
+        subject = "Nowa anonimowa wiadomość"
+
     send_email(
-        subject=f"Feedback - Nowa wiadomość od użytkownika {username} | {user_email}",
+        subject=subject,
         body_html=ready_to_send_user_msg,
         from_email='"KarmaSpace Feedback" <karmabot@karmaspace.io>',
         to_emails=["feedback@karmaspace.io"],
--- src/karmaspace/views/messages.py
+++ src/karmaspace/views/messages.py
@@ -1,11 +1,11 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework import serializers
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
 
-from src.app_auth.models import User
 from ..tasks import send_user_feedback
 
 
@@ -14,13 +14,16 @@ class PostInputSerializer(serializers.Serializer):
 
 
 @api_view(http_method_names=["POST"])
-@permission_classes([IsAuthenticated])
+@permission_classes([AllowAny])
 def user_feedback_view(request: Request) -> Response:
     serializer = PostInputSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
 
     dangerous_user_msg = serializer.validated_data["msg"]
-    user: User = request.user
-    send_user_feedback.send(user.username, user.email, dangerous_user_msg)
+
+    if isinstance(request.user, AnonymousUser):
+        send_user_feedback.send(dangerous_user_msg)
+    else:
+        send_user_feedback.send(dangerous_user_msg, str(request.user.pk))
 
     return Response(data={}, status=HTTP_201_CREATED)
